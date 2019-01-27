d085419e1b491cd79d1eb4927fff3815b5bfb391
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2019-01-27 09:12:53 +0700

    Updates

---
+++ precache-manifest.5157b63948b72f84cdc0b58527bbafd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "500dad1e5b39eff6e95b",
+    "url": "/ggj-2019-web/static/js/runtime~main.500dad1e.js"
+  },
+  {
+    "revision": "84ce843993504a9e25df",
+    "url": "/ggj-2019-web/static/js/main.84ce8439.chunk.js"
+  },
+  {
+    "revision": "cc5078140cb2a341dd70",
+    "url": "/ggj-2019-web/static/js/1.cc507814.chunk.js"
+  },
+  {
+    "revision": "84ce843993504a9e25df",
+    "url": "/ggj-2019-web/static/css/main.64480157.chunk.css"
+  },
+  {
+    "revision": "b8c09525a2bf200ea78325b26a9aad08",
+    "url": "/ggj-2019-web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ggj-2019-web/precache-manifest.de25716a4a2fee8c8ea19952411b0176.js"
+  "/ggj-2019-web/precache-manifest.5157b63948b72f84cdc0b58527bbafd6.js"
 );
 
 workbox.clientsClaim();
